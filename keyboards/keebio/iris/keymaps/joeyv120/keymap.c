fec42da3687e359dc84da60e2f7129b50f3a9222
joeyv120 <joeyv120@outlook.com>
2020-12-22 14:01:04 -0500

    Tweak to layer 1

--- keyboards/keebio/iris/keymaps/joeyv120/keymap.c
+++ keyboards/keebio/iris/keymaps/joeyv120/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                               XXXXXXX, KC_LEFT, XXXXXXX, KC_RGHT, XXXXXXX, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, KC_END,  KC_DOWN, KC_PGDN, XXXXXXX, KC_RSFT,
+     _______, XXXXXXX, XXXXXXX, XXXXXXX, MO(2),   KC_LALT, XXXXXXX,          XXXXXXX, XXXXXXX, KC_END,  KC_DOWN, KC_PGDN, XXXXXXX, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
